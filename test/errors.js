0ef0ba0eda769c631ea3048861b7e55f8a223ebb
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-15 00:33:02 +0800

    updated errors test

--- test/errors.js
+++ test/errors.js
@@ -7,27 +7,31 @@ chai.use(chaiHttp);
 const API_PREFIX = '/v1';
 
 describe('Errors', () => {
-  describe('/GET JSON 404 route', () => {
+  describe('GET JSON 404 route', () => {
     it('it should return error message', (done) => {
       chai.request(server)
         .get(API_PREFIX + '/404')
         .set('Accept', 'application/json')
         .end((err, res) => {
           res.should.have.status(404);
-          res.body.should.be.a('object');
-          res.body.should.have.property('error');
+          res.text.should.be.a('string');
+          let resData = JSON.parse(res.text);
+          resData.should.have.property('status');
+          resData.should.have.property('msg');
           done();
         });
     });
   });
 
-  describe('/GET HTML 406 route', () => {
+  describe('GET HTML 406 route', () => {
     it('it should return error message', (done) => {
       chai.request(server)
         .get(API_PREFIX + '/404')
         .set('Accept', 'text/html')
         .end((err, res) => {
           res.should.have.status(406);
+          res.text.should.be.a('string');
+          res.text.should.be.equals('Page not found');
           done();
         });
     });
