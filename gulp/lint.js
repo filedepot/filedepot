1cc75554ed9fb6885645527c87c5459cb03f8e95
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-14 18:01:22 +0800

    fixed gulp lint path

--- gulp/lint.js
+++ gulp/lint.js
@@ -2,7 +2,7 @@ const jshint = require('gulp-jshint');
 const gulp   = require('gulp');
 
 gulp.task('lint', function() {
-  return gulp.src('./src/*.js')
+  return gulp.src('./src/**/*.js')
     .pipe(jshint({ esversion: 6 }))
     .pipe(jshint.reporter('default'));
 });
