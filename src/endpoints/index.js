d3ce57bf93e9b6e5f5c21de6848b13a90cfa8928
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-14 17:57:48 +0800

    removed legacy cors code to depend on cors npm package

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -3,22 +3,6 @@ const path = require("path");
 module.exports = (app) => {
   let API_PREFIX = "/v1";
 
-  if (!process.env.CORS_CLIENT_DOMAIN) {
-    console.log('[WARN] The CORS_CLIENT_DOMAIN for Cross Origin Control is not set.')
-  }
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Max-Age', '86400');
-    res.header('Access-Control-Allow-Origin', process.env.CORS_CLIENT_DOMAIN || '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, Cache-Control, Pragma, Content-Type, Authorization');
-    res.header('Content-Type', 'text/plain');
-    if ('OPTIONS' === req.method) {
-      return res.status(200).send();
-    }
-    next();
-  });
-
   app.use((req, res, next) => {
     // overriding res.json so that we can send text/plain for cross origin requests
     res.json = (obj) => {
