4efd8d174e49c52ce03cb3b5b3f41bcd14ee8cb5
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-15 17:08:35 +0800

    fixed getToken with object name, ipAddress and parse response

--- test/getToken.js
+++ test/getToken.js
@@ -7,17 +7,18 @@ chai.use(chaiHttp);
 const API_PREFIX = '/v1';
 const ACCESS_KEY = 'ejAhvOzPP0OL.UbXRkwxDFNCvGRoXOrgOWDJKMv8imJi5CYci78mZJhghAWG9N7ZtWPyWIj1O';
 
-function getToken(method) {
+function getToken(method, object) {
   return new Promise((resolve, reject) => {
     chai.request(server)
       .post(API_PREFIX + '/tokens')
-      .set('authorization', KEY_TOKEN)
-      .send({ userAgent: 'UserAgent', method: method, ipAddress: '::1' })
+      .set('authorization', ACCESS_KEY)
+      .send({ userAgent: 'UserAgent', method: method, ipAddress: '::ffff:127.0.0.1', filename: object })
       .end((err, res) => {
         if (err) {
           return reject(new Error(err));
         }
-        resolve(res.body.token);
+        let resData = JSON.parse(res.text);
+        resolve(resData.result);
       });
   });
 };
