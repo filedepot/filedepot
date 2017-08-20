71f179e72cb6eb4fcc76e72c01eb5c4d6a3a9e41
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-21 00:02:35 +0800

    fix server.js of eslint complains

--- src/server.js
+++ src/server.js
@@ -1,53 +1,58 @@
 const http = require('http');
 const debug = require('debug')('server');
+const app = require('./app');
 
 module.exports = {
   start: (port) => {
-    const app = require('./app');
     app.set('port', port);
 
     var server = http.createServer(app);
-    server.on('error', (error) => { onError(error, port); });
-    server.on('listening', () => { onListening(server); });
+
+    server.on('error', (error) => {
+      onError(error, port);
+    });
+
+    server.on('listening', () => {
+      onListening(server);
+    });
+
     server.listen(port);
     return server;
   }
 };
 
-/**
+/*
  * Event listener for HTTP server "error" event.
  */
-
 function onError(error, port) {
   if (error.syscall !== 'listen') {
     throw error;
   }
 
-  var bind = typeof port === 'string' ? 'Pipe ' + port
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
     : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       debug(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
+      throw new Error('Insufficient privileges');
     case 'EADDRINUSE':
       debug(bind + ' is already in use');
-      process.exit(1);
-      break;
+      throw new Error('Port in use');
     default:
       throw error;
   }
 }
 
-/**
+/*
  * Event listener for HTTP server "listening" event.
  */
-
 function onListening(server) {
   var addr = server.address();
-  var bind = typeof addr === 'string' ? 'pipe ' + addr
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
