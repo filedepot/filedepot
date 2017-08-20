0a74afb9e1cbdf290851c19e8579dc8505a66885
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-20 21:34:49 +0800

    implement list subcommand

---
+++ bin/cli-list.js
@@ -0,0 +1,31 @@
+require('dotenv').config({ silent: true });
+const program = require('commander');
+const models = require('../src/models');
+require('console.table');
+
+program
+  .description('List all buckets managed by FileDepot')
+  .option('-l, --list', 'list bucket IDs only')
+  .parse(process.argv);
+
+
+models.Bucket
+  .findAll({
+    logging: false
+  })
+  .then((buckets) => {
+    if (program.list) {
+      buckets.forEach((bucket) => {
+        console.log(bucket.bucketId);
+      });
+      process.exit(0);
+      return
+    }
+
+    let resultTable = [];
+    buckets.forEach((bucket) => {
+      resultTable.push([bucket.bucketId, bucket.path]);
+    });
+    console.table(['Bucket ID', 'Path'], resultTable);
+    process.exit(0);
+  });
